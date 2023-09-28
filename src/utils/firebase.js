81bddff0b076b9114446bb738bd70dcddc80bf5c
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-09-28 09:15:44 +0530

    Updated Code

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,26 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE,
-//   authDomain: "blog-cddc2.firebaseapp.com",
-//   projectId: "blog-cddc2",
-//   storageBucket: "blog-cddc2.appspot.com",
-//   messagingSenderId: "413696194974",
-//   appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-
-
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -38,3 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Export storage references
+export const firebaseStorage = getStorage(app);
