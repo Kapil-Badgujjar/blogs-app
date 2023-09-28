5dfdbb2c4b7a0b43f4ff4d20c690ab4af8c59279
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-09-28 12:12:17 +0530

    Added url env variable

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,44 +1,58 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
-    cache: "no-store",
-  });
+  try {
+    const res = await fetch(process.env.APP_URL + "/api/categories", {
+      cache: "no-store",
+    });
 
-  if (!res.ok) {
-    throw new Error("Failed");
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return []; // Return an empty array in case of an error.
   }
-
-  return res.json();
 };
 
-const CategoryList = async () => {
-  const data = await getData();
+const CategoryList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData()
+      .then((result) => setData(result))
+      .catch((error) => console.error("Error setting data:", error));
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map((item) => (
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles[item.slug]}`}
-            key={item._id}
-          >
-            {item.img && (
-              <Image
-                src={item.img}
-                alt=""
-                width={32}
-                height={32}
-                className={styles.image}
-              />
-            )}
-            {item.title}
-          </Link>
-        ))}
+        {Array.isArray(data) &&
+          data.map((item) => (
+            <Link
+              href={`/blog?cat=${item.slug}`}
+              className={`${styles.category} ${styles[item.slug]}`}
+              key={item._id}
+            >
+              {item.img && (
+                <Image
+                  src={item.img}
+                  alt={item.title}
+                  width={32}
+                  height={32}
+                  className={styles.image}
+                />
+              )}
+              {item.title}
+            </Link>
+          ))}
       </div>
     </div>
   );
